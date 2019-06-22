2431801beed9efe4121d1f6f388256b759d12d3b
John Mansfield <jmansfield6@gatech.edu>
2019-06-22 06:58:11 -0400

    code cleanup

--- src/adult_nn.py
+++ src/adult_nn.py
@@ -1,25 +1,17 @@
-#nn build taken from pybrain.org
+#nn build from pybrain.org
 
-#from sklearn import tree
-#from sklearn import preprocessing
 import numpy as np
 from sklearn import preprocessing
 from sklearn import cross_validation
 from sklearn.metrics import accuracy_score
-#from sklearn import cross_validation
 import time
 from pybrain.datasets            import ClassificationDataSet
 from pybrain.utilities           import percentError
 from pybrain.tools.shortcuts     import buildNetwork
 from pybrain.supervised.trainers import BackpropTrainer
 from pybrain.structure.modules   import SoftmaxLayer
-from pylab import ion, ioff, figure, draw, contourf, clf, show, hold, plot
-from scipy import diag, arange, meshgrid, where
-from numpy.random import multivariate_normal
-import time
 
-
-
+#load dataset
 filtered_data=np.array(np.genfromtxt('..\\filtered-data.txt', delimiter=',',autostrip=True))
 scaled_filtered_data=preprocessing.scale(filtered_data)
 
@@ -27,64 +19,45 @@ scaled_filtered_data=preprocessing.scale(filtered_data)
 salary=scaled_filtered_data[:,14]
 scaled_filtered_data=scaled_filtered_data[:,:-1]
 
-print scaled_filtered_data.shape
-
 #set the target class to -1 or 1 for values less than 50 k or greater than 50 k respectively 
 salary2=np.array([-1]*len(salary))
 for i in range(0,len(salary2)):
     if salary[i]>0:
         salary2[i]=1
-		
 
-
-train_samples, test_samples, train_out, test_out = cross_validation.train_test_split(scaled_filtered_data, salary, test_size=.5, random_state=0)
-
-	
+#setup cross validation
+train_samples, test_samples, train_out, test_out = cross_validation.train_test_split(scaled_filtered_data, salary, test_size=.5, random_state=0)	
 X=train_samples
 y=train_out
-# cvX=test_samples
-# cvy=test_out
 
-
-#setup the ClassificationDataSet dimensions with 4 attributes petal width, petal height, sepal width, sepal height and the target values 
+#setup ClassificationDataSet dimensions
 ds = ClassificationDataSet(14, 1, class_labels=['>50K, <=50K'])
 cvds = ClassificationDataSet(14, 1, class_labels=['>50K, <=50K'])
 
-libsvm_start=time.time()
 #add data to ClassificationDataSet
+libsvm_start=time.time()
 for i in range(len(X)):
- ds.addSample(X[i],y[i])
- 
-# #add data to CV ClassificationDataSet
-# for i in range(len(cvX)):
- # cvds.addSample(cvX[i],cvy[i])
- 
+ ds.addSample(X[i],y[i]) 
  
 #split data then duplicates the original targets and stores them in an (integer) field named class.
 tstdata, trndata = ds.splitWithProportion( 0.25 )
 tstTargets = tstdata.getField('target')
 ds._convertToOneOfMany( ) 
 
-# cvTargets = cvds.getField('target')
-# cvds._convertToOneOfMany( ) 
-
-
-
-
-#print out a data sample 
+#print data sample 
 print "Number of training patterns: ", len(ds)
 print "Number of testing patterns: ", len(cvds)
 print "Input and output dimensions: ", ds.indim, ds.outdim
 print "First sample (input, target, class):"
 print ds['input'][0], ds['target'][0], ds['class'][0]
 
-#Now build a feed-forward network with 15 hidden units
+#build feed-forward network with 15 hidden units
 fnn = buildNetwork( ds.indim, 5, ds.outdim, outclass=SoftmaxLayer )
 
-#Set up a back propagation trainer that takes the network and training dataset as input.
+#Setup back propagation trainer that takes the network and training dataset as input.
 trainer = BackpropTrainer( fnn, dataset=ds, momentum=0.4, verbose=True, weightdecay=0.04)
 
-
+#train and print results
 for i in range(20):
    trainer.trainEpochs( 5 )
    trnresult = percentError( trainer.testOnClassData(), ds['class'] )
@@ -94,6 +67,6 @@ for i in range(20):
    tstresult = trainer.testOnClassData(dataset=tstdata)
    tstresult = np.array(tstresult)
    print "Accuracy on test set: %7.9f" % accuracy_score(tstTargets, tstresult, normalize=True)
- 
+
 libsvm_train_end=time.time()
 print "Time Required for training                  : ",str(libsvm_train_end-libsvm_start)+" s"
--- src/adult_pca.py
+++ src/adult_pca.py
@@ -1,19 +1,9 @@
-#data formatting partially taken from ipython.org
-
-
-
 import numpy as np 
 from sklearn import preprocessing
 from sklearn import cross_validation
-
-
 from sklearn import decomposition
-from sklearn import random_projection
-import matplotlib.pyplot as plt
-from scipy import signal
-import time
 
-#load the data
+#load data
 filtered_data=np.array(np.genfromtxt('..\\filtered-data.txt', delimiter=',',autostrip=True))
 scaled_filtered_data=preprocessing.scale(filtered_data)
 
@@ -21,29 +11,22 @@ scaled_filtered_data=preprocessing.scale(filtered_data)
 salary=scaled_filtered_data[:,14]
 scaled_filtered_data=scaled_filtered_data[:,:-1]
 
-print scaled_filtered_data.shape
-
 #set the target class to -1 or 1 for values less than 50 k or greater than 50 k respectively 
 salary2=np.array([-1]*len(salary))
 for i in range(0,len(salary2)):
     if salary[i]>0:
         salary2[i]=1
 		
-
+#setup cross validation
 train_samples, test_samples, train_out, test_out = cross_validation.train_test_split(scaled_filtered_data, salary2, test_size=0, random_state=0)
-
-
-
 X = train_samples
 y = train_out
 
-
-print X.shape
+#apply pca
 pca = decomposition.PCA(n_components=2)
 pca.fit(X)
 new_X = pca.transform(X)
-print new_X.shape
 
-
+#save data
 np.savetxt("attribute_pca_out.csv", new_X, delimiter=",")
 np.savetxt("target_pca_out.csv", y, delimiter=",")
